d916105df8c155bf677e999796d48c503b4bbb57
Radu Sologub <radu@sologub.md>
2024-06-12 12:38:12 +0300

    Added EnsureOldUserToNewUserDeserialization test.

---
+++ Tests/Types.fs
@@ -0,0 +1,60 @@
+﻿module Types
+
+open System
+
+type UserData = {
+    Tags: string array
+}
+
+[<CLIMutable>]
+type User = {
+    Username: string
+    Auth: bool
+    Banned: bool
+    FirstSeen: DateTimeOffset
+    LastSeen: DateTimeOffset
+    Data: UserData
+}
+
+let randomUsersToInsert = [|
+    {
+        Username = "John"
+        Auth = true
+        Banned = false
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -15 |> _.ToUnixTimeMilliseconds() |> DateTimeOffset.FromUnixTimeMilliseconds
+        LastSeen = DateTimeOffset.UtcNow.AddMinutes -10 |> _.ToUnixTimeMilliseconds() |> DateTimeOffset.FromUnixTimeMilliseconds
+        Data = {
+            Tags = [|"tag1-A"|]
+        }
+    };
+    {
+        Username = "Mihail"
+        Auth = true
+        Banned = false
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -10
+        LastSeen = DateTimeOffset.UtcNow.AddMinutes -8
+        Data = {
+            Tags = [|"tag2"|]
+        }
+    };
+    {
+        Username = "Vanya"
+        Auth = true
+        Banned = true
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -10
+        LastSeen = DateTimeOffset.UtcNow.AddMinutes -5
+        Data = {
+            Tags = [|"tag1-B"|]
+        }
+    };
+    {
+        Username = "Givany"
+        Auth = false
+        Banned = false
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -10
+        LastSeen = DateTimeOffset.UtcNow.AddMinutes -2
+        Data = {
+            Tags = [|"tag1-C"|]
+        }
+    }
+|]
---
+++ Tests/TypesOld.fs
@@ -0,0 +1,50 @@
+﻿module TypesOld
+
+open System
+
+[<CLIMutable>]
+type User = {
+    Username: string
+    Auth: bool
+    Banned: bool
+    FirstSeen: DateTimeOffset
+}
+
+let randomOldUsersToInsert = [|
+    {
+        Username = "David"
+        Auth = true
+        Banned = false
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -5 |> _.ToUnixTimeMilliseconds() |> DateTimeOffset.FromUnixTimeMilliseconds
+    };
+    {
+        Username = "Alex"
+        Auth = true
+        Banned = false
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -3 |> _.ToUnixTimeMilliseconds() |> DateTimeOffset.FromUnixTimeMilliseconds
+    };
+    {
+        Username = "Sam"
+        Auth = true
+        Banned = true
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -8 |> _.ToUnixTimeMilliseconds() |> DateTimeOffset.FromUnixTimeMilliseconds
+    };
+    {
+        Username = "Nicolay"
+        Auth = false
+        Banned = false
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -20 |> _.ToUnixTimeMilliseconds() |> DateTimeOffset.FromUnixTimeMilliseconds
+    };
+    {
+        Username = "Giorgio"
+        Auth = false
+        Banned = true
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -2 |> _.ToUnixTimeMilliseconds() |> DateTimeOffset.FromUnixTimeMilliseconds
+    };
+    {
+        Username = "Alfred"
+        Auth = true
+        Banned = false
+        FirstSeen = DateTimeOffset.UtcNow.AddYears -1 |> _.ToUnixTimeMilliseconds() |> DateTimeOffset.FromUnixTimeMilliseconds
+    }
+|]
