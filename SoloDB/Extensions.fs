be0a8932c6487d4b6e43002ecaf208f1962e6357
Radu <radu@sologub.md>
2025-05-28 23:21:06 +0300

    Removed F# Dynamic Nuget reference. Bump the Snappier version to 1.2.0. Shrank the JSON number stack buffer.

--- SoloDB/Extensions.fs
+++ SoloDB/Extensions.fs
@@ -6,7 +6,6 @@ open System.Linq
 open System.Text.RegularExpressions
 open System.Runtime.CompilerServices
 open System.Linq.Expressions
-open FSharp.Interop.Dynamic
 open System.Reflection
 
 [<Extension; AbstractClass; Sealed>]
@@ -22,17 +21,53 @@ type Extensions =
         let f = condition.Compile true
         this |> Seq.exists f.Invoke
 
-    [<Extension>]
-    static member Dyn<'T>(this: obj, property: string) : 'T = // LINQ Expressions do not support C# dynamic.
-        this |> Dyn.get<'T> property
+    // LINQ Expressions do not support C# dynamic.
 
+    /// <summary>
+    /// Dynamically retrieves a property value from an object and casts it to the specified generic type.
+    /// </summary>
+    /// <param name="this">The source object.</param>
+    /// <param name="property">The name of the property to retrieve.</param>
+    /// <returns>The value of the property cast to type 'T.</returns>
     [<Extension>]
-    static member Dyn<'T>(this: obj, property: PropertyInfo) : 'T = // LINQ Expressions do not support C# dynamic.
-        property.GetValue this :?> 'T
+    static member Dyn<'T>(this: obj, propertyName: string) : 'T =
+        let prop = this.GetType().GetProperty(propertyName, BindingFlags.Public ||| BindingFlags.Instance)
+        if prop <> null && prop.CanRead then
+            prop.GetValue(this) :?> 'T
+        else
+            let msg = sprintf "Property '%s' not found on type '%s' or is not readable." propertyName (this.GetType().FullName)
+            raise (ArgumentException(msg))
 
+    /// <summary>
+    /// Retrieves a property value from an object using a PropertyInfo object and casts it to the specified generic type.
+    /// </summary>
+    /// <param name="this">The source object.</param>
+    /// <param name="property">The PropertyInfo object representing the property to retrieve.</param>
+    /// <returns>The value of the property cast to type 'T.</returns>
     [<Extension>]
-    static member Dyn(this: obj, property: string) : obj =
-        this |> Dyn.get property
+    static member Dyn<'T>(this: obj, property: PropertyInfo) : 'T =
+        try
+            property.GetValue(this) :?> 'T
+        with
+        | :? InvalidCastException as ex ->
+            let msg = sprintf "Cannot cast property '%s' value to type '%s'." property.Name (typeof<'T>.FullName)
+            raise (InvalidCastException(msg, ex))
+
+    /// <summary>
+    /// Dynamically retrieves a property value from an object.
+    /// </summary>
+    /// <param name="this">The source object.</param>
+    /// <param name="property">The name of the property to retrieve.</param>
+    /// <returns>The value of the property as an obj.</returns>
+    [<Extension>]
+    static member Dyn(this: obj, propertyName: string) : obj =
+        let prop = this.GetType().GetProperty(propertyName, BindingFlags.Public ||| BindingFlags.Instance)
+        if prop <> null && prop.CanRead then
+            prop.GetValue(this)
+        else
+            let msg = sprintf "Property '%s' not found on type '%s' or is not readable." propertyName (this.GetType().FullName)
+            raise (ArgumentException(msg))
+
 
     [<Extension>]
     static member CastTo<'T>(this: obj) : 'T =
