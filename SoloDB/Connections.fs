d29185ab91b028b6ad90e1955049773e8e27d502
Radu <radu@sologub.md>
2025-06-02 00:37:17 +0300

    Added a SQL command caching mechanism. Removed the automatic pooling for SqliteConnection. Version Bump to 0.1.4.

--- SoloDB/Connections.fs
+++ SoloDB/Connections.fs
@@ -10,27 +10,6 @@ module Connections =
     open System.Threading.Tasks
     open Utils
 
-    /// Redirect all the method calls to the connection provided in the constructor, and on Dispose, it is a noop.
-    type DirectConnection internal (connection: IDbConnection) =
-        interface IDbConnection with
-            override this.BeginTransaction() = connection.BeginTransaction()
-            override this.BeginTransaction (il: IsolationLevel) = connection.BeginTransaction il
-            override this.ChangeDatabase (databaseName: string) = connection.ChangeDatabase databaseName
-            override this.Close() = connection.Close()
-            override this.CreateCommand() = connection.CreateCommand()
-            override this.Open() = connection.Open()
-            member this.ConnectionString with get() = connection.ConnectionString and set (cs) = connection.ConnectionString <- cs
-            member this.ConnectionTimeout: int = connection.ConnectionTimeout
-            member this.Database: string = connection.Database
-            member this.State: ConnectionState = connection.State
-
-        interface IDisposable with 
-            override this.Dispose (): unit = 
-                ()
-
-        member this.DisposeReal() =
-            connection.Dispose()
-
     type TransactionalConnection internal (connectionStr: string) =
         inherit SqliteConnection(connectionStr)
 
@@ -41,30 +20,21 @@ module Connections =
             // Noop
             ()
 
-    type PooledConnection internal (connectionStr: string, manager: ConnectionManager) =
-        inherit SqliteConnection(connectionStr)
-
-        member internal this.DisposeReal(disposing) =
-            base.Dispose disposing
-
-        override this.Dispose(disposing) =
-            manager.TakeBack this
-
     and ConnectionManager internal (connectionStr: string, setup: SqliteConnection -> unit) =
-        let all = ConcurrentStack<PooledConnection>()
-        let pool = ConcurrentStack<PooledConnection>()
+        let all = ConcurrentStack<CachingDbConnection>()
+        let pool = ConcurrentStack<CachingDbConnection>()
         let mutable disposed = false
 
         let checkDisposed () =
             if disposed then raise (ObjectDisposedException(nameof(ConnectionManager)))
 
-        member internal this.TakeBack(pooledConn: PooledConnection) =
+        member internal this.TakeBack(pooledConn: CachingDbConnection) =
             // SQLite does not support nested transactions, therefore we can use it to check if the user forgot to 
             // end the transaction before returning it to the pool.
             try pooledConn.Execute("BEGIN; ROLLBACK;") |> ignore
             with 
             | :? SqliteException as se when se.SqliteErrorCode = 1 && se.SqliteExtendedErrorCode = 1 ->
-                failwithf "The transaction must be finished before you return the connection to the pool."
+                ("The transaction must be finished before you return the connection to the pool.", se) |> InvalidOperationException |> raise
 
             pool.Push pooledConn
 
@@ -72,13 +42,13 @@ module Connections =
             checkDisposed()
             match pool.TryPop() with
             | true, c -> 
-                if c.State <> ConnectionState.Open then
-                    c.Open()
+                if c.Inner.State <> ConnectionState.Open then
+                    c.Inner.Open()
                 c
             | false, _ -> 
-                let c = new PooledConnection(connectionStr, this)
-                c.Open()
-                setup c
+                let c = new CachingDbConnection(new SqliteConnection(connectionStr), this.TakeBack)
+                c.Inner.Open()
+                setup c.Inner
                 all.Push c
                 c
 
@@ -89,7 +59,7 @@ module Connections =
             setup c
             c
 
-        member private this.WithTransactionBorrowed(f: SqliteConnection -> 'T) =
+        member private this.WithTransactionBorrowed(f: CachingDbConnection -> 'T) =
             use connectionForTransaction = this.Borrow()
             connectionForTransaction.Execute("BEGIN IMMEDIATE;") |> ignore
                 
@@ -101,7 +71,7 @@ module Connections =
                 connectionForTransaction.Execute "ROLLBACK;" |> ignore
                 reraise()
 
-        member private this.WithTransactionBorrowedAsync(f: SqliteConnection -> Task<'T>) = task {
+        member private this.WithTransactionBorrowedAsync(f: CachingDbConnection -> Task<'T>) = task {
             use connectionForTransaction = this.Borrow()
             connectionForTransaction.Execute("BEGIN IMMEDIATE;") |> ignore
                 
@@ -114,40 +84,10 @@ module Connections =
                 return reraiseAnywhere ex
         }
 
-        member private this.WithTransactionNewlyCreated(f: SqliteConnection -> 'T) =
-            use connectionForTransaction = this.CreateForTransaction()
-            try
-                connectionForTransaction.Execute("BEGIN IMMEDIATE;") |> ignore
-                
-                try
-                    let ret = f connectionForTransaction
-                    connectionForTransaction.Execute "COMMIT;" |> ignore
-                    ret
-                with ex -> 
-                    connectionForTransaction.Execute "ROLLBACK;" |> ignore
-                    reraise()
-            finally connectionForTransaction.DisposeReal(true)
-
-        member private this.WithTransactionNewlyCreatedAsync(f: SqliteConnection -> Task<'T>) = task {
-            use connectionForTransaction = this.CreateForTransaction()
-            try
-                connectionForTransaction.Execute("BEGIN IMMEDIATE;") |> ignore
-        
-                try
-                    let! ret = f connectionForTransaction
-                    connectionForTransaction.Execute "COMMIT;" |> ignore
-                    return ret
-                with ex -> 
-                    connectionForTransaction.Execute "ROLLBACK;" |> ignore
-                    return reraiseAnywhere ex
-            finally connectionForTransaction.DisposeReal(true)
-        }
-
-
-        member internal this.WithTransaction(f: SqliteConnection -> 'T) =
+        member internal this.WithTransaction(f: CachingDbConnection -> 'T) =
             this.WithTransactionBorrowed f
 
-        member internal this.WithAsyncTransaction(f: SqliteConnection -> Task<'T>) = task {
+        member internal this.WithAsyncTransaction(f: CachingDbConnection -> Task<'T>) = task {
             return! this.WithTransactionBorrowedAsync f
         }
 
@@ -155,7 +95,7 @@ module Connections =
             override this.Dispose() =
                 disposed <- true
                 for c in all do
-                    c.DisposeReal(true)
+                    c.DisposeReal()
                 all.Clear()
                 pool.Clear()
                 ()
